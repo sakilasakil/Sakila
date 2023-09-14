3952cc7c436f59bbd65be3ce87bf00f90d16b9d6
Sakila. N <144575011+sakilasakil@users.noreply.github.com>
2023-09-14 08:19:54 +0530

    Challenge 2.1.py

--- Sakila.py
+++ Sakila.py
@@ -1,19 +1,31 @@
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
+class BankAccount:
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
+    def __init__(self, account_number, account_holder_name, account_balance):
+        self.account_number = account_number
+        self.account_holder_name = account_holder_name
+        self._account_balance = account_balance  # private attribute
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
+    def deposit(self, amount):
+        self._account_balance += amount
 
-# Creating objects of the derived classes
-batsman = Batsman()
-bowler = Bowler()
+    def withdraw(self, amount):
+        if amount > self._account_balance:
+            raise ValueError("Insufficient funds")
+        self._account_balance -= amount
 
-# Calling the play() method for each object
-batsman.play()
-bowler.play()
+    def get_balance(self):
+        return self._account_balance
+
+
+def main():
+    account = BankAccount("123456789", "John Doe", 1000)
+
+    account.deposit(500)
+    print("Account balance:", account.get_balance())
+
+    account.withdraw(200)
+    print("Account balance:", account.get_balance())
+
+
+if __name__ == "__main__":
+    main()
